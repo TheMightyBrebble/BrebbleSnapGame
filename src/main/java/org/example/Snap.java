9c9265bc91f6f06b91a21768f84b77bbaeffb83a
bradley <braddersc@aol.com>
2026-05-22 09:56:14 +0100

    Snap class: checking dealt card against last card

--- src/main/java/org/example/Snap.java
+++ src/main/java/org/example/Snap.java
@@ -7,6 +7,15 @@ public class Snap extends CardGame{
     public void playGame() {
         boolean game = true;
         while (true) {
+            //Puts card removed from top of the deck into a variable and then prints it
+            //ISSUE WITH IF STATEMENT, NOT WORKING AS INTENDED DUE TO CARD IMMEDIATELY BEING ADDED TO DISCARDEDCARDS
+            Card dealtCard = this.dealCard();
+            System.out.println("Dealt card: " + dealtCard);
+                System.out.println("Last card drawn: " + discardedCards.getLast());
+                if (dealtCard.getSuit() == discardedCards.getLast().getSuit()) {
+                    System.out.println("Snap!!!");
+                    break;
+                }
 
             //Code for continuing the game after a card is revealed
             System.out.println("Press Enter Key to Continue.");
