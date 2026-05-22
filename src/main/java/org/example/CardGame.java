c9600ac9db12906a3e9c28ce5815f345c95d1051
bradley <braddersc@aol.com>
2026-05-22 09:51:14 +0100

    CardGame Class: deckbuilding + getting top card

---
+++ src/main/java/org/example/CardGame.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.*;
+
+public class CardGame {
+    ArrayList<Card> deckOfCards = new ArrayList<Card>();
+    ArrayList<Card> discardedCards = new ArrayList<Card>();
+    public CardGame() {
+        makeDeck();
+        shuffleDeck();
+    }
+
+    private void makeDeck() {
+        for (Suit suit : Suit.values()) {
+            int value = 2;
+            for(Symbol symbol : Symbol.values()) {
+                deckOfCards.add(new Card(suit, symbol, value));
+                value++;
+            }
+        }
+    }
+    public void getDeck(){
+        for (Card card : deckOfCards) {
+            System.out.println(card);
+        }
+    }
+
+
+    public Card dealCard() {
+        if (deckOfCards.isEmpty()) {
+            throw new IllegalStateException("No cards remaining in this deck");
+        }
+        Card discarded = deckOfCards.removeFirst();
+        discardedCards.add(discarded);
+        return discarded;
+    }
+
+
+
+    public void shuffleDeck() {
+        Collections.shuffle(deckOfCards);
+    }
+
+}
